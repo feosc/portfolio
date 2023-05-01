8e716e6de886c52ca1a9d2c8d0ee10bb291f1396
feosc <lipeosc@gmail.com>
2023-05-01 16:53:38 -0300

    Open AI

---
+++ components/Slogan.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="slogan">
+        <h1>NutriPal</h1>
+        <span>O caminho certo para uma dieta adaptada às suas necessidades!</span>
+    </div>
+</template>
+
+<style scoped>
+.slogan {
+    display: grid;
+    grid-template-rows: 1fr, 1fr;
+    grid-gap: 2rem;
+}
+
+h1 {
+    align-self: self-end;
+    font-size: 54px;
+}
+
+span {
+    font-size: 34px;
+    font-weight: 500;
+}
+</style>
